6673f9fe578bbe22c9cc97f74c3397272f736242
joaoveronezi <veronezijoao01@gmail.com>
2020-06-01 11:02:37 -0300

    Mockup

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./HomeContent.scss";
 
+import Mockup from "../Mockup";
+
 const HomeContent = () => {
-  return <div className="teste"></div>;
+  return (
+    <div className="HomeContent">
+      <div className="aside">
+        <Mockup />
+      </div>
+      <main>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </h1>
+      </main>
+    </div>
+  );
 };
 
 export default HomeContent;
