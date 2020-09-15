690a53b2320e71f5d6baf107364a848efaa7b7fe
joaoveronezi <veronezijoao01@gmail.com>
2020-09-14 21:04:04 -0300

    animacoes

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import "./HomeContent.scss";
 import cellphone from "../../assets/images/Component3.png";
 import arrow from "../../assets/images/seta.gif";
+import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
 
 const HomeContent = () => {
   return (
     <div className="HomeContent">
       <div className="Lside">
-        <img
-          src={cellphone}
-          href="/#"
-          className="cellphoneDir"
-          alt="cellphone-mockup"
-        ></img>
+        <Slide bottom>
+          <img
+            src={cellphone}
+            href="/#"
+            className="cellphoneDir"
+            alt="cellphone-mockup"
+          />
+        </Slide>
       </div>
-      <div className="Rside">
-        <p>
-          Não é sobre <strong>dinheiro</strong>, mas sim sobre{" "}
-          <strong>sonhos!</strong>
-        </p>
-        <h6>#VemProAzul </h6>
-      </div>
-      <a href="#quemsomos">
-        <img src={arrow} className="arrow" alt="Arrow pointing down"></img>
-      </a>
+      <Fade bottom delay={1000}>
+        <div className="Rside">
+          <p>
+            Não é sobre <strong>dinheiro</strong>, mas sim sobre{" "}
+            <strong>sonhos!</strong>
+          </p>
+          <h6>#VemProAzul </h6>
+        </div>
+        <a href="#quemsomos">
+          <img src={arrow} className="arrow" alt="Arrow pointing down"></img>
+        </a>
+      </Fade>
     </div>
   );
 };
